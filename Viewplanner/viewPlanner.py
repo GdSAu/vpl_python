2b9a0d641717f2387b1077b9c1613c329cb8379e
Irving Vasquez <jirvingvg@gmail.com>
2025-03-24 13:58:18 -0600

    add partial model class

--- Viewplanner/viewPlanner.py
+++ Viewplanner/viewPlanner.py
@@ -1,23 +1,13 @@
 
 
-class ViewPlanner:
+class NBVPlanner:
+    def __init__(self, robot_sensor, partial_model):
+        self.robot_sensor = robot_sensor
+        self.partial_model = partial_model
 
-    def __init__(self, method):
-        self.method = self.__loadmethod()
-
-    def __loadmethod(self):
-        #here we will load the corresponding method
+    def PlanNBV(self):
+        # Code to plan the next best view
         pass
 
 
-    def forward(self, i, direccion):
-        
-        """
-        we recieve direction to the data then the method access to the requiered data
-        and return a prediction
-
-        How to do it for multiple returned parameters f.e. [x,y,z] o [R|t] o class?
-
-        """
-        nbv = self.method.nbv()
-        return nbv
+    
